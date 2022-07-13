be52bd9a43ad58ba725917fd1d94c417e377dc29
SebaGimenez <sebastiangimenezlopezen@gmail.com>
2022-07-13 19:56:37 -0300

    armado de navBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  const styleApp = { padding: "10'x 20px", marginTop: 10}
   return (
-    <div className="App">
+
+    <div className="App" style={styleApp}>
+
+      <NavBar />
       <h1>COMISION 34685</h1>
       <h2> CODERHOUSE </h2>
       <p> Proyecto ReactApp</p>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+
+
+const NavBar = () => {
+    return(
+        <div>
+        <h1>logo</h1>
+        <ul>
+            <li><button>Inicio</button></li>
+            <li><button>Productos</button></li>
+            <li><button>Sobre Nosotros</button></li>
+            <li><button>Contacto</button></li>
+        </ul>
+    </div>
+    )
+   
+
+}
+export default NavBar;
