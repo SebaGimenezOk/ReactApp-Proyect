b6dad01df9773c6155b7800d378262bab5e9fb52
SebaGimenez <sebastiangimenezlopezen@gmail.com>
2022-07-16 15:22:54 -0300

    correccion de nombre project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-
-import "./components/NavBootstrap/NavBootstrap.scss"
+import "./components/NavBootstrap/NavBootstrap.scss";
 // import "./App.scss";
 // import NavBar from "./components/NavBar/NavBar";
 import NavBootstrap from "./components/NavBootstrap/NavBootstrap.js";
 
-
-
-
 function App() {
-  const styleApp = { padding: "5px 2px", marginTop: 3}
+  const styleApp = { padding: "5px 2px", marginTop: 3 };
   return (
-
     <div className="App-header" style={styleApp}>
-         <NavBootstrap/> 
+      <NavBootstrap />
       {/* <NavBar /> */}
       <h1>COMISION 34685</h1>
       <h2> CODERHOUSE </h2>
